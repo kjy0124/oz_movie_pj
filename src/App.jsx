e1b953732b9c873a59b24fe17f846761373d2d76
kjy0124 <0124kimjy@naver.com>
2025-03-10 00:26:16 +0900

    🚀 Set up routing(Main, MovieDetail)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 // App.jsx
-import { useState } from "react";
-import movieListData from "./assets/data/movieListData.json";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import MovieDetail from "./pages/MovieDetail";
@@ -8,14 +6,10 @@ import Main from "./pages/Main";
 import Layout from "./components/Layout";
 
 function App() {
-  //json파일을 통해 가져온 데이터를 사용하여 movieList의 상태를 관리
-  //초기 값울 movieListData의 영화 목록 데이터로 설정
-  const [movieList, setMovieList] = useState(movieListData.results);
-
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="/home" element={<Main movieList={movieList} />} />
+        <Route path="/home" element={<Main />} />
         <Route path="/details" element={<MovieDetail />} />
       </Route>
     </Routes>
