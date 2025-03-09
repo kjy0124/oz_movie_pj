52041fe90ddaec9a1edca361192051a931435fbe
kjy0124 <0124kimjy@naver.com>
2025-03-10 00:28:24 +0900

    💄 NavBar 스타일 수정 및 추가

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,37 @@
+import { useNavigate } from "react-router-dom";
+
 function NavBar() {
-  return <div>NavBar</div>;
+  const nav = useNavigate();
+
+  const goHome = () => {
+    nav("/home");
+  };
+
+  return (
+    <div className="w-full h-[40px] px-6 flex mb-3 justify-between">
+      <h2 onClick={goHome} className="font-bold cursor-pointer text-2xl">
+        Oz_KJY
+      </h2>
+
+      {/* 검색창과 버튼 - flex로 중앙 정렬 */}
+      {/* space-x-2 x축으로 간격 8px 추가 */}
+      <div className="flex w-1/2 justify-center items-center space-x-2">
+        <input
+          type="sub"
+          placeholder="영화 검색..."
+          className="w-2/3 p-2 rounded-lg text-black"
+        />
+        <button className="px-4 py-2 bg-blue-500 hover:bg-blue-600 rounded-lg">
+          검색
+        </button>
+      </div>
+      {/* flexbox, 요소 간 간격 4, 자동 마진으로 우측 정렬 */}
+      <div className="flex space-x-4">
+        <button>Login</button>
+        <button>SignUp</button>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
